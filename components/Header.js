e5efb62950b70777ee40f665ab12e3234a3431a8
심현보 <hbsim0605@gmail.com>
2025-11-02 20:51:15 +0900

    헤더 수정

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,18 @@
-
+import Link from "next/link";
 
 const Header = () => {
   return (
-    <header className="bg-white dark:bg-slate-700 z-50 sticky top-0 right-0  w-full flex justify-between items-center px-6 py-4">
-      <h1 className="text-5xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-yellow-300 to-pink-500  dark:text-white-tracking-wide">
+    <header className="bg-white dark:bg-slate-700 z-50 sticky top-0 right-0 w-full flex justify-between items-center px-6 py-4">
+      {/* 제목 클릭 시 홈으로 이동 */}
+      <Link
+        href="/"
+        className="text-5xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-yellow-300 to-pink-500 dark:text-white tracking-wide hover:opacity-80 transition-opacity"
+      >
         GENIUS
-      </h1>
+      </Link>
 
-      {/* DarkModeToggleButton을 오른쪽 끝으로 정렬 */}
-      <div className="flex justify-start">
-       
-      </div>
+      {/* 오른쪽 끝 영역 */}
+      <div className="flex justify-start">{/* 여기에 버튼들 */}</div>
     </header>
   );
 };
